af88826eb715380348582f935c9efdbc41dee9ec
Kolade Faith <koladefaith1000@gmail.com>
2025-08-18 09:23:58 +0100

    Work done

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,6 +19,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+        <link rel="apple-touch-icon" href="/public/icons/asset 9.png" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
